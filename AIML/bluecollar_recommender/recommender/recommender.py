3776695097c4eea731bfd7a3028584f4a33c38cd
Mrunal Talikoti <mrunaltalikoti2@gmail.com>
2025-07-22 19:00:37 +0530

    Delete AIML directory

--- AIML/bluecollar_recommender/recommender/recommender.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity
-
-def recommend(worker_index, worker_vecs, company_vecs, companies, top_n=3):
-    similarities = cosine_similarity(worker_vecs[worker_index], company_vecs).flatten()
-    top_matches = similarities.argsort()[::-1][:top_n]
-    return companies.iloc[top_matches], similarities[top_matches]
