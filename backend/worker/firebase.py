a6469bfbb6d25f0e4e1aa1a49a605a3c1b90e932
Shivanikinagi <shivani.kinagi@gmail.com>
2025-07-29 18:52:29 +0530

    Remove all Firebase dependencies and related files - project no longer uses Firebase

--- backend/worker/firebase.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, auth
-
-cred = credentials.Certificate("C:/Users/shivani/OneDrive/Desktop/innohack_card/project/backend/worker-2261e-firebase-adminsdk-fbsvc-868ba499bf.json")
-firebase_admin.initialize_app(cred)
